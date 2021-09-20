dd77144e80ba8f2ae0339cd0ac3b53287d280771
hachi2k <91079410+hachi2k@users.noreply.github.com>
2021-09-20 23:05:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Happy Birthday",
+    introTitle: 'Phạm Thùy Bống',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao Pống cứ đi nhầm đường
+    Lạc vào tim Chee lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
+    btnIntro: 'Bấm thì bấm hong bấm thì bấm',
+    title: 'Chúc mừng bạn đã chọn vào ô bấm tiếp',
+    desc: 'Thì nói chung là bấm tiếp đi ',
+    btnYes: 'Iu Pống 3k <333',
+    btnNo: 'Iu Pống 7749 <3333',
+    question: 'Trên thế giới có 8 tỉ người nhưng Pống là duy nhất',
+    btnReply: 'Câu cé thì dốt nhưng iu Pống é ',
+    reply: 'Sinh nhựt zui zẻ <3',
+    mess: 'Có tâm thì bank cho Chee 500k nha😘',
+    messDesc: 'Vì sinh nhật Pống nên Chee thật xinh đẹp :* .',
+    btnAccept: 'Nói chung là lớp riuuu:**',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
